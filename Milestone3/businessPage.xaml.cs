83eae15517a52ba91fcf39a5d5bea47d8c6d0938
Jomar Dimaculangan <jomaridimaculangan@gmail.com>
2018-04-21 17:46:24 -0700

    Added general queries.

--- Milestone3/businessPage.xaml.cs
+++ Milestone3/businessPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using Npgsql;
 
 namespace Milestone3 {
     /// <summary>
@@ -20,26 +22,83 @@ namespace Milestone3 {
     public partial class businessPage : Page {
         public businessPage() {
             InitializeComponent();
+            populateStates();
+            populateCat();
         }
 
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
+        private string buildConnString() {
+            return "Host=localhost; Username=postgres; Password=db2018; Database=yelpdb; port=8181";
+        }
+
+        //Populate states:
+        void populateStates() {
+            using (var conn = new NpgsqlConnection(buildConnString())) {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand()) {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT distinct state FROM business";
+                    using (var reader = cmd.ExecuteReader()) {
+                        while (reader.Read()) {
+                            stateBox.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+        }
+
+        //populate categories:
+        void populateCat() {
+            using (var conn = new NpgsqlConnection(buildConnString())) {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand()) {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT distinct catname FROM businesscat ORDER BY catname;";
+                    using (var reader = cmd.ExecuteReader()) {
+                        while (reader.Read()) {
+                            buscatBox.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+        }
+
+        //clicking on a state:
+        private void stateBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            cityBox.Items.Clear();
+            using (var conn = new NpgsqlConnection(buildConnString())) {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand()) {
+                    cmd.Connection = conn;
+                    if (stateBox.SelectedValue != null) {
+                        cmd.CommandText = "SELECT distinct city FROM business WHERE state = '" + stateBox.SelectedValue.ToString() + "'";
+                        using (var reader = cmd.ExecuteReader()) {
+                            while (reader.Read()) {
+                                cityBox.Items.Add(reader.GetString(0));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void Add_Click(object sender, RoutedEventArgs e) {
 
         }
 
-        private void Remove_Click(object sender, RoutedEventArgs e)
-        {
+        private void Remove_Click(object sender, RoutedEventArgs e) {
 
         }
 
-        private void Search_Businesses_Click(object sender, RoutedEventArgs e)
-        {
+        private void Search_Businesses_Click(object sender, RoutedEventArgs e) {
 
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) {
 
         }
+
+        
     }
 }
